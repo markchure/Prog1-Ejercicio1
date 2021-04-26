6786f874d4f9b29fa3554c993b259e396ef45bef
Marcos Churi <51281680+markchure@users.noreply.github.com>
2021-04-26 01:44:42 -0300

    Update AreaTriangulo.js

--- AreaTriangulo.js
+++ AreaTriangulo.js
@@ -1,3 +1,4 @@
+//Pedir la base y altura de un triángulo. Mostrar su área. Ej. si se lee 6 y 2, muestra 6.
 var base = parseInt(prompt("Ingrese la base: "));
 var altura = parseInt(prompt("Ingrese la altura: "));
 var area = base*altura/2;
